cfb096b4513ede68a262b5ba50a2c0e01312efb1
hafiz huzaifa <56879849+hafizhuzaifabit@users.noreply.github.com>
2022-10-20 12:50:23 +0500

    auth pages added

--- medicine/lib/main.dart
+++ medicine/lib/main.dart
@@ -49,33 +49,6 @@ class MyApp extends StatelessWidget {
         routes: {
           //define routes here
         },
-      ),
-    return GetMaterialApp(
-      builder: (context, child) => ResponsiveWrapper.builder(
-          BouncingScrollWrapper.builder(context, child!),
-          maxWidth: 1200,
-          minWidth: 450,
-          defaultScale: true,
-          breakpoints: [
-            const ResponsiveBreakpoint.resize(450, name: MOBILE),
-            const ResponsiveBreakpoint.autoScale(800, name: TABLET),
-            const ResponsiveBreakpoint.autoScale(1000, name: TABLET),
-            const ResponsiveBreakpoint.resize(1200, name: DESKTOP),
-            const ResponsiveBreakpoint.autoScale(2460, name: "4K"),
-          ],
-          background: Container(color: const Color(0xFFF5F5F5))),
-      title: 'Medicine',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        fontFamily: "Open Sans",
-        backgroundColor: Colors.white,
-        scaffoldBackgroundColor: Colors.white,
-      ),
-      scrollBehavior: MyCustomScrollBehavior(),
-      home: MyHomePage(),
-      routes: {
-        //define routes here
-      },
-    );
+      ));
   }
 }
